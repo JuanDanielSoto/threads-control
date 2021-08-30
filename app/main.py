83301695a0b03ec5792c4617e6e1a20fe5c161c1
JuanDanielSoto <halogeno321@gmail.com>
2021-08-30 16:22:24 -0500

    first commit

---
+++ app/main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify, redirect, url_for, render_template, flash
+from datetime import datetime
+from sharedTools import *
+from ach import *
+from flask_cors import CORS
+import numpy as np
+
+app= Flask(__name__)
+DB = database("db")
+log = database("login")
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+
+
+#Iot de CO2s
+
+@app.route("/") #Actualizada
+def all():
+      return jsonify(DB.load())
+
+@app.route("/new/<string:sen>") #Actualizado
+def postNewSensor(sen):
+      db = DB.load()
+      if exist(sen, db):
+            return jsonify({"error": "'"+sen+"' alrredy exists"})
+      else:
+            db[sen] = {"central":{"DATE":[],"HUM":[],"PPM":[],"TEMP":[]},"lateral":{"DATE":[],"HUM":[],"PPM":[],"TEMP":[]}}
+            DB.update(db)
+            return jsonify({"sucess": "'"+sen+"' fue creado correctamente"})
+
+@app.route("/update", methods=["PUT"]) #Actualizado
+def updateValues():
+      """
+      request - 
+      {
+        "name": "nombre",
+        "site": "ubicación"
+        "series": {
+            "HUM": [],
+            "PPM": [],
+            "TEMP": []
+          }
+      }
+      """
+      post = request.json
+      db = DB.load()
+      if exist(post["name"], db):
+            now = datetime.now()
+            db[post["name"]][post["site"]]["PPM"].append(post["series"]["PPM"])
+            db[post["name"]][post["site"]]["HUM"].append(post["series"]["HUM"])
+            db[post["name"]][post["site"]]["TEMP"].append(post["series"]["TEMP"])
+            db[post["name"]][post["site"]]["DATE"].append(str(now.date())+"/"+str(now.time()))
+            DB.update(db)
+            return jsonify({"sucess": "'"+post["name"]+"' fue falta actualizar correctamente"})
+      else:
+            return jsonify({"error": "'"+post["name"]+"' does not exists"})
---
+++ upload.sh
@@ -0,0 +1,6 @@
+pip3 freeze
+pip3 freeze > requirements.txt
+git add .
+git commit -m "automatic push"
+git push
+git pull
