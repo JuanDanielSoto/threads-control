e79a38eb789139a357add18399d291c897c06f87
JuanDanielSoto <halogeno321@gmail.com>
2021-08-31 06:10:15 -0500

    automatic push

--- app/main.py
+++ app/main.py
@@ -43,3 +43,55 @@ def updateValues():
             return jsonify({"status":"success"})
       except:
             return jsonify({"status":"403 Bad request"})
+
+#Access login
+
+@app.route("/login/validate", methods=["POST"])
+def validate():
+      """
+      request - 
+      {
+            "user": "name",
+            "pwd" : "password"
+      }
+      """
+      post = request.json
+      users = log.load()
+      for user in users:
+            if post["user"] == user["user"]:
+                  if post["pwd"] == user["pwd"]:
+                        return jsonify({"error": "Acceso concedido", "acess": True})
+                  else:
+                        return jsonify({"error": "Contraseña incorrecta", "acess": False})
+      return jsonify({"error": "Usuario no existe", "acess": False})
+
+@app.route("/login/add", methods=["POST"])
+def addUser():
+      """
+      request - 
+      {
+            "user": "name"
+      }
+      """
+      post = request.json
+      users = log.load()
+      for user in users:
+            if post["user"] == user["user"]:
+                  return jsonify({"error": "Usuario ya existe"})
+      try:
+            users.append({"user": post["user"], "pwd" : post["pwd"]})
+            log.update(users)
+            return jsonify({"success": "Usuario registrado exitosamente"})
+      except Exception:
+            return jsonify({"error": "Entrada inválida"})
+
+@app.route("/login/delete", methods=["DELETE"])
+def deleteUser():
+      post = request.json
+      users = log.load()
+      for i, user in enumerate(users):
+            if post["user"] == user["user"]:
+                  users.pop(i)
+                  log.update(users)
+                  return jsonify({"success": "Usuario eliminado correctamente"})
+      return jsonify({"error": "Este usuario no existe"})
